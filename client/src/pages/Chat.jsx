b0f6268fd38f8b28643afc226f09d9309e2fcfa7
tarik.fi <tarik42777@gmail.com>
2023-05-03 02:43:24 +0300

    chat start

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000");
 
 function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const messageInput = useRef(null);
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message !== "") {
+      socket.emit("sendMessage", message);
+      setMessage("");
+      messageInput.current.focus();
+    }
+  };
+
   return (
     <div className="flex h-screen  p-12 justify-center items-center">
       <div className="flex h-full antialiased text-gray-800 w-3/4">
@@ -69,13 +91,17 @@ function Chat() {
                   <div className="flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full">
                     P
                   </div>
-                  <div className="ml-2 text-sm font-semibold">Philip Tucker</div>
+                  <div className="ml-2 text-sm font-semibold">
+                    Philip Tucker
+                  </div>
                 </button>
                 <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
                   <div className="flex items-center justify-center h-8 w-8 bg-pink-200 rounded-full">
                     C
                   </div>
-                  <div className="ml-2 text-sm font-semibold">Christine Reid</div>
+                  <div className="ml-2 text-sm font-semibold">
+                    Christine Reid
+                  </div>
                 </button>
                 <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
                   <div className="flex items-center justify-center h-8 w-8 bg-purple-200 rounded-full">
@@ -105,56 +131,54 @@ function Chat() {
               <div className="flex flex-col h-full overflow-x-auto mb-4">
                 <div className="flex flex-col h-full">
                   <div className="grid grid-cols-12 gap-y-2">
-                    <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                      <div className="flex flex-row items-center">
-                        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                          A
-                        </div>
-                        <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                          <div>Hey How are you today?</div>
+                    {messages.map((msg, index) => (
+                      <div
+                        key={index}
+                        className={`${
+                          msg.isOwn
+                            ? "col-start-6 col-end-13"
+                            : "col-start-1 col-end-8"
+                        } p-3 rounded-lg`}
+                      >
+                        <div
+                          className={`${
+                            msg.isOwn
+                              ? "flex items-center justify-start flex-row-reverse"
+                              : "flex flex-row items-center"
+                          }`}
+                        >
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                            {msg.isOwn ? "Y" : "A"}
+                          </div>
+                          <div
+                            className={`${
+                              msg.isOwn ? "mr-3" : "ml-3"
+                            } relative text-sm ${
+                              msg.isOwn ? "bg-indigo-100" : "bg-white"
+                            } py-2 px-4 shadow rounded-xl`}
+                          >
+                            <div>{msg.text}</div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-
-                    <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                      <div className="flex items-center justify-start flex-row-reverse">
-                        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                          A
-                        </div>
-                        <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                          <div>I'm ok what about you?</div>
-                        </div>
-                      </div>
-                    </div>
+                    ))}
                   </div>
                 </div>
               </div>
               <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
-                <div>
-                  <button className="flex items-center justify-center text-gray-400 hover:text-gray-600">
-                    <svg
-                      className="w-5 h-5"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
                 <div className="flex-grow ml-4">
                   <div className="relative w-full">
                     <input
                       type="text"
+                      ref={messageInput}
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
                       className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
                     />
-                    <button className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+                    <button
+                      onClick={sendMessage}
+                      className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
+                    >
                       <svg
                         className="w-6 h-6"
                         fill="none"
@@ -163,9 +187,9 @@ function Chat() {
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         ></path>
                       </svg>
